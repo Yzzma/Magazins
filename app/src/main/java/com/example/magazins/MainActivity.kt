75b3e4c007ef889d1cbe3404f40cf3d1793d72e7
kalilov_m <marlenkalilov86@gmail.com>
2021-12-21 14:03:25 +0600

    first magazin

---
+++ app/src/main/java/com/example/magazins/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.magazins
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.R
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
